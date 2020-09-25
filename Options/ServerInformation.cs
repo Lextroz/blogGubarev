394b427a1439e57834cddab0f5ff8bdb621dfd14
Lextroz <gveka@list.ru>
2020-09-25 21:23:48 +0300

    Добавьте файлы проекта.

---
+++ Options/ServerInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogGubarev.Options
+{
+    public class ServerInformation
+    {
+        public string host = "localhost";
+        public long port = 5001;
+    }
+}
